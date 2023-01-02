29c871eae2455c44cfe1aea58c63b173f92271c9
Rodri Otrera <rodrigo.otrerafornes@gmail.com>
2023-01-02 03:27:59 -0300

    CourseIDpage

--- src/pages/course/[courseId].tsx
+++ src/pages/course/[courseId].tsx
@@ -3,6 +3,7 @@ import type { InferGetServerSidePropsType, NextPage } from "next";
 import MainLayout from "../../../components/home/MainLayout";
 import { type GetServerSidePropsContext } from "next";
 import Navbar from "../../../components/home/Navbar";
+import Image from "next/image";
 
 const Course: NextPage<CourseServerSideProps> = ({ params }) => {
   console.log(params);
@@ -12,7 +13,27 @@ const Course: NextPage<CourseServerSideProps> = ({ params }) => {
       <MainLayout>
         <Navbar />
       </MainLayout>
-      <div className="aspect-[18/6] bg-lightContainer px-6 py-7"></div>
+      <div className="relative flex aspect-[18/6] flex-col justify-end opacity-80">
+        <Image
+          className="absolute object-cover"
+          fill={true}
+          alt="Image"
+          src="/imagen-prueba-2.jpg"
+        />
+        <div className="relative flex flex-row items-end justify-between bg-transparent">
+          <div className="flex-col  p-4">
+            <h1 className="mb-2 text-3xl font-bold text-white">
+              Curso instructor
+            </h1>
+            <h1>Duracion: 10 modulos</h1>
+          </div>
+          <div className="flex flex-col  items-center rounded-tl-xl bg-[rgb(0,0,0,0.8)] p-4 ">
+            <h1>Comprar Curso</h1>
+            <h1>240000</h1>
+            <h1>Medios de pago</h1>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
